025535e31fb2084ff9d64b746f8c9f2e504c1cc1
iotanum <evalduuk@gmail.com>
2019-02-22 20:22:11 +0200

    Add test landing page/configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
     );
   }
