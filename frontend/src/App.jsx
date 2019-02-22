502d348bd560817ca19cf9ebe56b9e60df03836f
iotanum <evalduuk@gmail.com>
2019-02-22 22:54:03 +0200

    Add fetch for users endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import config from '../src/config'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state= {
+      users: []
+    }
+  }
+  componentDidMount() {
+    fetch(`${config.apiUrl}/users`)
+      .then(response => {
+        const promise = response.json()
+        promise.then(value => {
+          this.setState({users: value}, () => {
+            console.log(this.state.users)
+          })
+        })
+      })
+  }
+  
   render() {
     return (
       <div className="App">
