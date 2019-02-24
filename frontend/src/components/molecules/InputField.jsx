c259c56422132701525728f8c138ccf28e73155f
iotanum <evalduuk@gmail.com>
2019-02-24 16:44:47 +0200

    Implement fully working validations with rules for editing and adding a user

--- frontend/src/components/molecules/InputField.jsx
+++ frontend/src/components/molecules/InputField.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-export const InputField = ({ id, label, name, value, type, error }) => (
+export const InputField = ({ id, label, name, value, type, error, rules }) => (
     <fieldset>
         <label className="control-label" htmlFor={name}>{label}</label>
-        <input className={`input-field form-control ${error ? 'invalid' : ''}`} id={id} name={name} defaultValue={value} type={type}></input>
+        <input className={`input-field form-control ${error ? 'invalid' : ''}`} data-rules={rules} id={id} name={name} defaultValue={value} type={type}></input>
         <div className='error-message-wrapper'><span className='error-message'>{error}</span></div>
     </fieldset>
 )
--- frontend/src/resources/utils/validations.js
+++ frontend/src/resources/utils/validations.js
@@ -1,12 +1,13 @@
-const rules = {
+const rulesRegex = {
     required: /^$|\s+/,
     name: /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{1,20}$/,
     email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     zipcode: /^\d{5}(?:[-\s]\d{4})?$/,
-    geo: /^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/,
+    geo: /^-?([1-8]?[0-9]\.{1}\d{1,6}$|90\.{1}0{1,6}$)/,
     phone: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
-    website: /@^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$@i/,
-    text: /^(?=.{5,30}$)[a-z][a-z0-9]*(?:_[a-z0-9]+)*$/
+    website: /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/,
+    text: /^[a-zA-Z0-9_ ]*$/,
+    textLength: /\w{1,10}\b/
 }
 
 const errors = {
@@ -17,9 +18,55 @@ const errors = {
     geo: 'Wrong coordinates format!',
     phone: 'Phone format is wrong!',
     website: 'Website format is wrong!',
-    text: 'Too much characters!',
+    text: 'This field cannot contain special characters!',
+    textLength: 'Too many characters!'
+}
+
+const validateField = (field) =>
+{
+    const rules = field.getAttribute('data-rules').split(',')
+    const value = field.value
+    const fieldErrors = []
+    if (rules)
+    {
+        rules.forEach(rule => {
+            if (rule === 'required')
+            {
+                if (!value) fieldErrors.push(errors.required)
+            }
+            else {
+                if (!rulesRegex[rule].test(value))
+                {
+                    fieldErrors.push(errors[rule])
+                }
+            }
+        }) 
+    }
+    return fieldErrors
+}
+
+export const getFormData = formId => {
+    let dataObject = {}
+    const form = document.getElementById(formId)
+    const fields = form.getElementsByTagName('input')
+    for (let field of fields) {
+        dataObject = {...dataObject, [field.id]: field.value}
+    }
+    return dataObject
 }
 
 export const validateForm = formId => {
-    console.log(document.getElementById(formId))
+    let errors = {}
+    const form = document.getElementById(formId)
+    const fields = form.getElementsByTagName('input')
+    for (let field of fields) {
+        const errorsTemp = validateField(field)
+
+        if (errorsTemp.length !== 0) {
+            errors={...errors, [field.id]: errorsTemp}
+        }
+          
+    }
+
+    return errors
 }
