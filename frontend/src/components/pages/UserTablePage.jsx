14f123bb97ecd5d3b6caf633013527bc779bbe8b
iotanum <evalduuk@gmail.com>
2019-02-24 21:14:44 +0200

    Implemented latest endpoints in buttons, added endpoint response header

--- frontend/src/components/pages/UserTablePage.jsx
+++ frontend/src/components/pages/UserTablePage.jsx
@@ -8,7 +8,8 @@ class UsersTablePage extends Component {
   constructor(props) {
     super(props)
     this.state= {
-      users: []
+      users: [],
+      response: ''
     }
   }
   componentDidMount() {
@@ -25,23 +26,47 @@ class UsersTablePage extends Component {
       })
   }
   
-  seedUsers = () => {
-    fetch(`${config.apiUrl}/seed`)
+  seedUsers = (seedOne) => {
+    fetch(`${config.apiUrl}/seed/${seedOne}`)
       .then(response => {
         const promise = response.json()
         promise.then(value => {
+          this.setState({response: value})
           this.updateUsers()
         })
       })
+  }
+
+  seedTen = () => {
+    this.seedUsers('False')
+  }
+
+  seedOne = () => {
+    this.seedUsers('True')
+  }
+
+  cleanTable = () => {
+    fetch(`${config.apiUrl}/drop`)
+      .then(response => {
+        const promise = response.json()
+        promise.then(value => {
+          this.setState({response: value})
+          this.updateUsers()
+      })
+    })
   }
 
   render() {
     return (
       <div className="App">
         <div className="usersTableWrapper">
+        <h2>{this.state.response}</h2>
           <div className="users-table-button-wrapper">
             <Link to={`/user/add`}><button type="button" className="btn btn-success add-button">Add</button></Link>
-            <button type="button" onClick={this.seedUsers} className="btn btn-success seed-button">Seed</button>
+            <div className="users-table-seed-clean-buttons-wrapper">
+              <button type="button" onClick={this.cleanTable} className="btn btn-success clean-button">Clean</button>
+              <button type="button" onClick={this.seedTen} className="btn btn-success seed-button">Seed-10</button>
+            </div>
           </div>
           <div className="usersTableContentWrapper">
             <UsersTable users={this.state.users} updateUsers={this.updateUsers}/>
